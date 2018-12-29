18ca053d01045100997cd5b499caa7d06a59814a
zk124 <jeelpatel1612sd@gmail.com>
2018-12-29 20:35:20 +1400

    modified ul*.js files(which runs userLogic)

--- js/ulmf.js
+++ js/ulmf.js
@@ -1,425 +1,101 @@
 // main func. i/p Runner
+// (NEW)
 function runUl() {
+	var ulMain=document.getElementById("mt").innerHTML;
+	if (runClk <= ulMain) {
+		var mou = "ul" + runClk;
+		var myUl=document.getElementById(mou).src;
+		var myUlLen=myUl.length;
+		var myUlLenFi=myUlLen-10;
+		var myUlFi=myUl.substring(myUlLenFi,myUlLen);
 
-	if(runClk==1){
+			if(myUlFi === 'img/f1.png'){
+				runUlf1();
+			}
+			else if(myUlFi === 'img/f2.png'){
+				runUlf2();
+			}
+			else if(myUlFi === 'img/up.png'){
+				moFwdRun();
+			}
+			else if(myUlFi === 'img/rl.png'){
+				moRlRun();
+			}
+			else if(myUlFi === 'img/rr.png'){
+				moRrRun();
+			}
+			else if(myUlFi === 'img/gr.png'){
+				moGrRun();
+			}
+			runClk++;
+			setTimeout(runUl, 500);
+	}
+}
 
-	var myUl=document.getElementById('ul1').src;
-	var myUlLen=myUl.length;
-	var myUlLenFi=myUlLen-10;
-	var myUlFi=myUl.substring(myUlLenFi,myUlLen);
+// func.1 i/p Runner
+// compressed loops (NEW)
+function runUlf1() {
+	var ulF1=document.getElementById("f1t").innerHTML;
+	if(runClk1 <= ulF1){
+      var mou = "1ul" + runClk1;
+      var myUl=document.getElementById(mou).src;
+    	var myUlLen=myUl.length;
+    	var myUlLenFi=myUlLen-10;
+    	var myUlFi=myUl.substring(myUlLenFi,myUlLen);
 
-		if(myUlFi === 'img/f1.png'){
-			runUlf1();
-		}
-		else if(myUlFi === 'img/f2.png'){
-			runUlf2();
-		}
-		else if(myUlFi === 'img/up.png'){
-			moFwdRun();
-		}
-		else if(myUlFi === 'img/rl.png'){
-			moRlRun();
-		}
-		else if(myUlFi === 'img/rr.png'){
-			moRrRun();
-		}
-		else if(myUlFi === 'img/gr.png'){
-			moGrRun();
-		}
-		runClk++;
-		setTimeout(runUl, 500);
-	}
-	else if(runClk==2){
+    		if(myUlFi === 'img/f1.png'){
+    			runUlf1();
+    		}
+    		else if(myUlFi === 'img/f2.png'){
+    			runUlf2();
+    		}
+    		else if(myUlFi === 'img/up.png'){
+    			moFwdRun();
+    		}
+    		else if(myUlFi === 'img/rl.png'){
+    			moRlRun();
+    		}
+    		else if(myUlFi === 'img/rr.png'){
+    			moRrRun();
+    		}
+    		else if(myUlFi === 'img/gr.png'){
+    			moGrRun();
+    		}
+    		runClk1++;
+        setTimeout(runUlf1, 500);
+  }
+}
 
-	var myUl=document.getElementById('ul2').src;
-	var myUlLen=myUl.length;
-	var myUlLenFi=myUlLen-10;
-	var myUlFi=myUl.substring(myUlLenFi,myUlLen);
+// func.2 i/p Runner
+// (NEW)
+function runUlf2() {
+	var ulF2=document.getElementById("f2t").innerHTML;
+	if (runClk2 <= ulF2) {
+		var mou = "2ul" + runClk2;
+		var myUl=document.getElementById(mou).src;
+		var myUlLen=myUl.length;
+		var myUlLenFi=myUlLen-10;
+		var myUlFi=myUl.substring(myUlLenFi,myUlLen);
 
-	if(myUlFi === 'img/f1.png'){
-		runUlf1();
-	}
-	else if(myUlFi === 'img/f2.png'){
-		runUlf2();
-	}
-	else if(myUlFi === 'img/up.png'){
-		moFwdRun();
-	}
-	else if(myUlFi === 'img/rl.png'){
-		moRlRun();
-	}
-	else if(myUlFi === 'img/rr.png'){
-		moRrRun();
-	}
-	else if(myUlFi === 'img/gr.png'){
-		moGrRun();
-	}
-		runClk++;
-		setTimeout(runUl, 500);
-	}
-	else if(runClk==3){
-
-	var myUl=document.getElementById('ul3').src;
-	var myUlLen=myUl.length;
-	var myUlLenFi=myUlLen-10;
-	var myUlFi=myUl.substring(myUlLenFi,myUlLen);
-
-	if(myUlFi === 'img/f1.png'){
-		runUlf1();
-	}
-	else if(myUlFi === 'img/f2.png'){
-		runUlf2();
-	}
-	else if(myUlFi === 'img/up.png'){
-		moFwdRun();
-	}
-	else if(myUlFi === 'img/rl.png'){
-		moRlRun();
-	}
-	else if(myUlFi === 'img/rr.png'){
-		moRrRun();
-	}
-	else if(myUlFi === 'img/gr.png'){
-		moGrRun();
-	}
-		runClk++;
-		setTimeout(runUl, 500);
-	}
-	else if(runClk==4){
-
-	var myUl=document.getElementById('ul4').src;
-	var myUlLen=myUl.length;
-	var myUlLenFi=myUlLen-10;
-	var myUlFi=myUl.substring(myUlLenFi,myUlLen);
-
-	if(myUlFi === 'img/f1.png'){
-		runUlf1();
-	}
-	else if(myUlFi === 'img/f2.png'){
-		runUlf2();
-	}
-	else if(myUlFi === 'img/up.png'){
-		moFwdRun();
-	}
-	else if(myUlFi === 'img/rl.png'){
-		moRlRun();
-	}
-	else if(myUlFi === 'img/rr.png'){
-		moRrRun();
-	}
-	else if(myUlFi === 'img/gr.png'){
-		moGrRun();
-	}
-		runClk++;
-		setTimeout(runUl, 500);
-	}
-	else if(runClk==5){
-
-	var myUl=document.getElementById('ul5').src;
-	var myUlLen=myUl.length;
-	var myUlLenFi=myUlLen-10;
-	var myUlFi=myUl.substring(myUlLenFi,myUlLen);
-
-	if(myUlFi === 'img/f1.png'){
-		runUlf1();
-	}
-	else if(myUlFi === 'img/f2.png'){
-		runUlf2();
-	}
-	else if(myUlFi === 'img/up.png'){
-		moFwdRun();
-	}
-	else if(myUlFi === 'img/rl.png'){
-		moRlRun();
-	}
-	else if(myUlFi === 'img/rr.png'){
-		moRrRun();
-	}
-	else if(myUlFi === 'img/gr.png'){
-		moGrRun();
-	}
-		runClk++;
-		setTimeout(runUl, 500);
-	}
-	else if(runClk==6){
-
-	var myUl=document.getElementById('ul6').src;
-	var myUlLen=myUl.length;
-	var myUlLenFi=myUlLen-10;
-	var myUlFi=myUl.substring(myUlLenFi,myUlLen);
-
-	if(myUlFi === 'img/f1.png'){
-		runUlf1();
-	}
-	else if(myUlFi === 'img/f2.png'){
-		runUlf2();
-	}
-	else if(myUlFi === 'img/up.png'){
-		moFwdRun();
-	}
-	else if(myUlFi === 'img/rl.png'){
-		moRlRun();
-	}
-	else if(myUlFi === 'img/rr.png'){
-		moRrRun();
-	}
-	else if(myUlFi === 'img/gr.png'){
-		moGrRun();
-	}
-		runClk++;
-		setTimeout(runUl, 500);
-	}
-	else if(runClk==7){
-
-	var myUl=document.getElementById('ul7').src;
-	var myUlLen=myUl.length;
-	var myUlLenFi=myUlLen-10;
-	var myUlFi=myUl.substring(myUlLenFi,myUlLen);
-
-	if(myUlFi === 'img/f1.png'){
-		runUlf1();
-	}
-	else if(myUlFi === 'img/f2.png'){
-		runUlf2();
-	}
-	else if(myUlFi === 'img/up.png'){
-		moFwdRun();
-	}
-	else if(myUlFi === 'img/rl.png'){
-		moRlRun();
-	}
-	else if(myUlFi === 'img/rr.png'){
-		moRrRun();
-	}
-	else if(myUlFi === 'img/gr.png'){
-		moGrRun();
-	}
-		runClk++;
-		setTimeout(runUl, 500);
-	}
-	else if(runClk==8){
-
-	var myUl=document.getElementById('ul8').src;
-	var myUlLen=myUl.length;
-	var myUlLenFi=myUlLen-10;
-	var myUlFi=myUl.substring(myUlLenFi,myUlLen);
-
-	if(myUlFi === 'img/f1.png'){
-		runUlf1();
-	}
-	else if(myUlFi === 'img/f2.png'){
-		runUlf2();
-	}
-	else if(myUlFi === 'img/up.png'){
-		moFwdRun();
-	}
-	else if(myUlFi === 'img/rl.png'){
-		moRlRun();
-	}
-	else if(myUlFi === 'img/rr.png'){
-		moRrRun();
-	}
-	else if(myUlFi === 'img/gr.png'){
-		moGrRun();
-	}
-		runClk++;
-		setTimeout(runUl, 500);
-	}
-	else if(runClk==9){
-
-	var myUl=document.getElementById('ul9').src;
-	var myUlLen=myUl.length;
-	var myUlLenFi=myUlLen-10;
-	var myUlFi=myUl.substring(myUlLenFi,myUlLen);
-
-	if(myUlFi === 'img/f1.png'){
-		runUlf1();
-	}
-	else if(myUlFi === 'img/f2.png'){
-		runUlf2();
-	}
-	else if(myUlFi === 'img/up.png'){
-		moFwdRun();
-	}
-	else if(myUlFi === 'img/rl.png'){
-		moRlRun();
-	}
-	else if(myUlFi === 'img/rr.png'){
-		moRrRun();
-	}
-	else if(myUlFi === 'img/gr.png'){
-		moGrRun();
-	}
-		runClk++;
-		setTimeout(runUl, 500);
-	}
-	else if(runClk==10){
-
-	var myUl=document.getElementById('ul10').src;
-	var myUlLen=myUl.length;
-	var myUlLenFi=myUlLen-10;
-	var myUlFi=myUl.substring(myUlLenFi,myUlLen);
-
-	if(myUlFi === 'img/f1.png'){
-		runUlf1();
-	}
-	else if(myUlFi === 'img/f2.png'){
-		runUlf2();
-	}
-	else if(myUlFi === 'img/up.png'){
-		moFwdRun();
-	}
-	else if(myUlFi === 'img/rl.png'){
-		moRlRun();
-	}
-	else if(myUlFi === 'img/rr.png'){
-		moRrRun();
-	}
-	else if(myUlFi === 'img/gr.png'){
-		moGrRun();
-	}
-		runClk++;
-		setTimeout(runUl, 500);
-	}
-	else if(runClk==11){
-
-	var myUl=document.getElementById('ul11').src;
-	var myUlLen=myUl.length;
-	var myUlLenFi=myUlLen-10;
-	var myUlFi=myUl.substring(myUlLenFi,myUlLen);
-
-	if(myUlFi === 'img/f1.png'){
-		runUlf1();
-	}
-	else if(myUlFi === 'img/f2.png'){
-		runUlf2();
-	}
-	else if(myUlFi === 'img/up.png'){
-		moFwdRun();
-	}
-	else if(myUlFi === 'img/rl.png'){
-		moRlRun();
-	}
-	else if(myUlFi === 'img/rr.png'){
-		moRrRun();
-	}
-	else if(myUlFi === 'img/gr.png'){
-		moGrRun();
-	}
-		runClk++;
-		setTimeout(runUl, 500);
-	}
-	else if(runClk==12){
-
-	var myUl=document.getElementById('ul12').src;
-	var myUlLen=myUl.length;
-	var myUlLenFi=myUlLen-10;
-	var myUlFi=myUl.substring(myUlLenFi,myUlLen);
-
-	if(myUlFi === 'img/f1.png'){
-		runUlf1();
-	}
-	else if(myUlFi === 'img/f2.png'){
-		runUlf2();
-	}
-	else if(myUlFi === 'img/up.png'){
-		moFwdRun();
-	}
-	else if(myUlFi === 'img/rl.png'){
-		moRlRun();
-	}
-	else if(myUlFi === 'img/rr.png'){
-		moRrRun();
-	}
-	else if(myUlFi === 'img/gr.png'){
-		moGrRun();
-	}
-		runClk++;
-		setTimeout(runUl, 500);
-	}
-	else if(runClk==13){
-
-	var myUl=document.getElementById('ul13').src;
-	var myUlLen=myUl.length;
-	var myUlLenFi=myUlLen-10;
-	var myUlFi=myUl.substring(myUlLenFi,myUlLen);
-
-	if(myUlFi === 'img/f1.png'){
-		runUlf1();
-	}
-	else if(myUlFi === 'img/f2.png'){
-		runUlf2();
-	}
-	else if(myUlFi === 'img/up.png'){
-		moFwdRun();
-	}
-	else if(myUlFi === 'img/rl.png'){
-		moRlRun();
-	}
-	else if(myUlFi === 'img/rr.png'){
-		moRrRun();
-	}
-	else if(myUlFi === 'img/gr.png'){
-		moGrRun();
-	}
-		runClk++;
-		setTimeout(runUl, 500);
-	}
-	else if(runClk==14){
-
-	var myUl=document.getElementById('ul14').src;
-	var myUlLen=myUl.length;
-	var myUlLenFi=myUlLen-10;
-	var myUlFi=myUl.substring(myUlLenFi,myUlLen);
-
-	if(myUlFi === 'img/f1.png'){
-		runUlf1();
-	}
-	else if(myUlFi === 'img/f2.png'){
-		runUlf2();
-	}
-	else if(myUlFi === 'img/up.png'){
-		moFwdRun();
-	}
-	else if(myUlFi === 'img/rl.png'){
-		moRlRun();
-	}
-	else if(myUlFi === 'img/rr.png'){
-		moRrRun();
-	}
-	else if(myUlFi === 'img/gr.png'){
-		moGrRun();
-	}
-		runClk++;
-		setTimeout(runUl, 500);
-	}
-	else if(runClk==15){
-
-	var myUl=document.getElementById('ul15').src;
-	var myUlLen=myUl.length;
-	var myUlLenFi=myUlLen-10;
-	var myUlFi=myUl.substring(myUlLenFi,myUlLen);
-
-	if(myUlFi === 'img/f1.png'){
-		runUlf1();
-	}
-	else if(myUlFi === 'img/f2.png'){
-		runUlf2();
-	}
-	else if(myUlFi === 'img/up.png'){
-		moFwdRun();
-	}
-	else if(myUlFi === 'img/rl.png'){
-		moRlRun();
-	}
-	else if(myUlFi === 'img/rr.png'){
-		moRrRun();
-	}
-	else if(myUlFi === 'img/gr.png'){
-		moGrRun();
-	}
-		//runClk++;
-		//setTimeout(runUl, 500);
-		runClk=1; // initilizing , max 15 i/ps only
+			if(myUlFi === 'img/f1.png'){
+				runUlf1();
+			}
+			else if(myUlFi === 'img/f2.png'){
+				runUlf2();
+			}
+			else if(myUlFi === 'img/up.png'){
+				moFwdRun();
+			}
+			else if(myUlFi === 'img/rl.png'){
+				moRlRun();
+			}
+			else if(myUlFi === 'img/rr.png'){
+				moRrRun();
+			}
+			else if(myUlFi === 'img/gr.png'){
+				moGrRun();
+			}
+			runClk2++;
+			setTimeout(runUlf2, 500);
 	}
 }
